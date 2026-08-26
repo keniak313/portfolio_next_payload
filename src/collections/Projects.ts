3833e0521012f70fa1592e9d66a3c092b48a8b0d
Pawel <keniak313@gmail.com>
2026-08-26 22:56:13 +0200

    up

---
+++ src/collections/Projects.ts
@@ -0,0 +1,19 @@
+import { CollectionConfig } from 'payload'
+
+export const Projects: CollectionConfig = {
+  slug: 'projects',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+  ],
+}
